dd3d6da085038c1d4aec800af87f98f52d080bc6
Abhishek Pawar <abhipawarraipur@gmail.com>
2024-12-01 06:33:25 -0800

    booleanType

---
+++ booleanType.ts
@@ -0,0 +1,20 @@
+// boolean 
+
+let  nameAbhi:boolean = true;
+
+let nameJoen:boolean = false;
+
+// write a typescript function called isEven that takes a number as a parameter and return true if the number is even and false otherwise
+
+const isEvenNum = (data:number) => {
+    return data % 2 === 0 ? true : false;
+}
+
+
+const EvenNum = (datas:number) => {
+    return datas % 2 === 0 ? true : false;
+}
+
+console.log(isEvenNum(2))
+
+
