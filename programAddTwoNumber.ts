a155cbeb40b7d7204214285e1a2257df070ebc04
Abhishek Pawar <abhipawarraipur@gmail.com>
2024-12-01 06:24:27 -0800

    typeAnnotations

---
+++ programAddTwoNumber.ts
@@ -0,0 +1,7 @@
+//write a program to add two number 
+
+const sum = (a:number,b:number):number =>{
+    return a + b ;
+}
+
+console.log(sum(65,54)); 
---
+++ typeAnnotations.ts
@@ -0,0 +1,22 @@
+// type annotations
+
+let myName:string = `Abhishek Paar`;
+
+let myFavNum:number = 25;
+
+let Intro:string = `Hello My Name is ${myName} and my favourite Number is ${myFavNum}`;
+
+console.log(Intro);
+
+let nameLength:number = myName.length;
+
+console.log(`length of name is ${nameLength}`);
+
+// uppercased & lowercased
+
+let IntreoUppercase:string = Intro.toUpperCase();
+
+let Introtolowercase:string = Intro.toLowerCase();
+
+console.log(IntreoUppercase)
+console.log(Introtolowercase)
