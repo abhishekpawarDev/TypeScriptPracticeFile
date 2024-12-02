4e32abc5dbcb55e6169b1475332532238bd67b07
Abhishek Pawar <abhipawarraipur@gmail.com>
2024-12-01 20:53:21 -0800

    any&unknownType

---
+++ any&unKnownType.ts
@@ -0,0 +1,23 @@
+//any type 
+
+let data:any = 'ABCD';
+
+data = 22;
+
+console.log(data)
+
+//unknown type
+
+let newData:unknown;
+
+newData = 'Abhishek';
+
+console.log(newData)
+
+newData = 13;
+
+console.log(newData)
+
+newData = true;
+
+console.log(newData)
