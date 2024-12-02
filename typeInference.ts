dcb15f1d5702b80e95dcaeca84fc069583bdf116
Abhishek Pawar <abhipawarraipur@gmail.com>
2024-12-01 21:38:34 -0800

    typeInference

---
+++ typeInference.ts
@@ -0,0 +1,4 @@
+//Type inference
+
+// Type inference in TypeScript is a feature where the TypeScript compiler automatically deduces the type of a variable, function, or expression based on the value assigned to it or its usage. This reduces the need for explicit type annotations while still maintaining type safety.
+
